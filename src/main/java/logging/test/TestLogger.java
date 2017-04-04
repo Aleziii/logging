4dadac7fdfaafa2c7fb89b5e10c8b0f425467f8e
Alezi Jose dos Santos <alezi.js@gmail.com>
2017-04-04 20:42:56 -0300

    Logging with sfl4j

---
+++ src/main/java/logging/test/TestLogger.java
@@ -0,0 +1,13 @@
+package logging.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestLogger { 
+	private static final Logger LOGGER = LoggerFactory.getLogger(TestLogger.class);
+	
+	public static void main(String args[]){
+		LOGGER.debug("Debug");
+		LOGGER.info("Info");
+	}
+}
